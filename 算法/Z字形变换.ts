c3afb704ccec3bc2db79f1c9606517998a7027d8
yanquankun <yanquankun@58.com>
2025-07-28 18:57:11 +0800

    feat: test

--- 算法/Z字形变换.ts
+++ 算法/Z字形变换.ts
@@ -57,3 +57,4 @@ console.log(convert("PAYPALISHIRING", 3)); // PAHNAPLSIIGYIR
 console.log(convert("PAYPALISHIRING", 4)); // PINALSIGYAHRPI
 console.log(convert("AB", 1)); // AB
 //====== 测试区域结束 ======
+// 1
